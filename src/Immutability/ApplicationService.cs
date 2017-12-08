5e69cea1bd80b96ba7a8d186850f41b5b9ade2bc
Rodrigo Sabino <rsabino@viajanet.com.br>
2017-12-08 14:36:24 -0200

    Exemplo de Código

---
+++ src/Immutability/ApplicationService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Immutability
+{
+    public class ApplicationService
+    {
+        private readonly string _directoryName;
+        private readonly AuditManagerNew _auditManager;
+        private readonly Persister _persister;
+
+        public ApplicationService(string directoryName)
+        {
+            _directoryName = directoryName;
+            _auditManager = new AuditManagerNew(10);
+            _persister = new Persister();
+        }
+
+        public void RemoveMentionsAbout(string visitorName)
+        {
+            FileContent[] files = _persister.ReadDirectory(_directoryName);
+            IReadOnlyList<FileAction> actions = _auditManager.RemoveMentionsAbout(visitorName, files);
+            _persister.ApplyChanges(actions);
+        }
+
+        public void AddRecord(string visitorName, DateTime timeOfVisit)
+        {
+            FileInfo fileInfo = new DirectoryInfo(_directoryName)
+                .GetFiles()
+                .OrderByDescending(x => x.LastWriteTime)
+                .First();
+
+            FileContent file = _persister.ReadFile(fileInfo.Name);
+            FileAction action = _auditManager.AddRecord(file, visitorName, timeOfVisit);
+            _persister.ApplyChange(action);
+        }
+    }
+}
